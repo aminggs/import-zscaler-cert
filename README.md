8752c341d779dfabb29bf54a44200e29889e67d9
Andrew Inggs <andrewinggs@capitecbank.co.za>
2023-03-31 13:49:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # import-zscaler-cert
-Shell script to import zscaler root CA certificate into various trust stores
+
+Mac shell script to import zscaler root CA certificate into various trust stores
+
+## Usage
+
+```shell
+./import-zscaler-cert
+```
+
+No arguments required, the root CA cert is embedded in the script.
+
+## Description
+
+Saves the Zscaler root CA cert to ~/.certs/Zscaler-Root-CA.pem and
+updates the following trust stores:
+
+* JDK key stores for all JDKs listed by `/usr/libexec/java_home -V`
+* JetBrains applications (`~/Library/Application\ Support/JetBrains/*`)
+* DBeaver (`/Applications/DBeaver.app`)
+
+## License
+
+Licensed under the Apache License, Version 2.0, unless otherwise
+explicitly stated. See file LICENSE for details.
---
+++ import-zscaler-cert.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#
+#   Copyright Andrew M. Inggs
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# Import Zscaler root CA cert into to trust stores.
+#
+# * JDK key stores for all JDKs listed by /usr/libexec/java_home -V
+# * JetBrains applications
+# * DBeaver
+
+if [ ! -d ~/.certs ]
+then
+	mkdir ~/.certs
+fi
+
+
+if [ ! -f ~/.certs/Zscaler-Root-CA.pem ]
+then
+	cat > ~/.certs/Zscaler-Root-CA.pem <<-EOF
+		-----BEGIN CERTIFICATE-----
+		MIIE0zCCA7ugAwIBAgIJANu+mC2Jt3uTMA0GCSqGSIb3DQEBCwUAMIGhMQswCQYD
+		VQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTERMA8GA1UEBxMIU2FuIEpvc2Ux
+		FTATBgNVBAoTDFpzY2FsZXIgSW5jLjEVMBMGA1UECxMMWnNjYWxlciBJbmMuMRgw
+		FgYDVQQDEw9ac2NhbGVyIFJvb3QgQ0ExIjAgBgkqhkiG9w0BCQEWE3N1cHBvcnRA
+		enNjYWxlci5jb20wHhcNMTQxMjE5MDAyNzU1WhcNNDIwNTA2MDAyNzU1WjCBoTEL
+		MAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExETAPBgNVBAcTCFNhbiBK
+		b3NlMRUwEwYDVQQKEwxac2NhbGVyIEluYy4xFTATBgNVBAsTDFpzY2FsZXIgSW5j
+		LjEYMBYGA1UEAxMPWnNjYWxlciBSb290IENBMSIwIAYJKoZIhvcNAQkBFhNzdXBw
+		b3J0QHpzY2FsZXIuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+		qT7STSxZRTgEFFf6doHajSc1vk5jmzmM6BWuOo044EsaTc9eVEV/HjH/1DWzZtcr
+		fTj+ni205apMTlKBW3UYR+lyLHQ9FoZiDXYXK8poKSV5+Tm0Vls/5Kb8mkhVVqv7
+		LgYEmvEY7HPY+i1nEGZCa46ZXCOohJ0mBEtB9JVlpDIO+nN0hUMAYYdZ1KZWCMNf
+		5J/aTZiShsorN2A38iSOhdd+mcRM4iNL3gsLu99XhKnRqKoHeH83lVdfu1XBeoQz
+		z5V6gA3kbRvhDwoIlTBeMa5l4yRdJAfdpkbFzqiwSgNdhbxTHnYYorDzKfr2rEFM
+		dsMU0DHdeAZf711+1CunuQIDAQABo4IBCjCCAQYwHQYDVR0OBBYEFLm33UrNww4M
+		hp1d3+wcBGnFTpjfMIHWBgNVHSMEgc4wgcuAFLm33UrNww4Mhp1d3+wcBGnFTpjf
+		oYGnpIGkMIGhMQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTERMA8G
+		A1UEBxMIU2FuIEpvc2UxFTATBgNVBAoTDFpzY2FsZXIgSW5jLjEVMBMGA1UECxMM
+		WnNjYWxlciBJbmMuMRgwFgYDVQQDEw9ac2NhbGVyIFJvb3QgQ0ExIjAgBgkqhkiG
+		9w0BCQEWE3N1cHBvcnRAenNjYWxlci5jb22CCQDbvpgtibd7kzAMBgNVHRMEBTAD
+		AQH/MA0GCSqGSIb3DQEBCwUAA4IBAQAw0NdJh8w3NsJu4KHuVZUrmZgIohnTm0j+
+		RTmYQ9IKA/pvxAcA6K1i/LO+Bt+tCX+C0yxqB8qzuo+4vAzoY5JEBhyhBhf1uK+P
+		/WVWFZN/+hTgpSbZgzUEnWQG2gOVd24msex+0Sr7hyr9vn6OueH+jj+vCMiAm5+u
+		kd7lLvJsBu3AO3jGWVLyPkS3i6Gf+rwAp1OsRrv3WnbkYcFf9xjuaf4z0hRCrLN2
+		xFNjavxrHmsH8jPHVvgc1VD0Opja0l/BRVauTrUaoW6tE+wFG5rEcPGS80jjHK4S
+		pB5iDj2mUZH1T8lzYtuZy0ZPirxmtsk3135+CKNa2OCAhhFjE0xd
+		-----END CERTIFICATE-----
+		EOF
+fi
+
+for dir in $(/usr/libexec/java_home -V 2>&1 | grep -v "Matching Java Virtual Machines" | awk '{ print $NF }' | sort | uniq)
+do
+	case "${dir}" in
+		*[^0-9][78]* )
+			sudo ${dir}/jre/bin/keytool -keystore ${dir}/jre/lib/security/cacerts -trustcacerts -import -alias Zscaler-Root-CA -file ~/.certs/Zscaler-Root-CA.pem -storepass changeit -noprompt
+			;;
+		* )
+			sudo ${dir}/bin/keytool -cacerts -importcert -alias Zscaler-Root-CA -file ~/.certs/Zscaler-Root-CA.pem -storepass changeit -noprompt
+			;;
+	esac
+done
+
+for keystore in $(ls ~/Library/Application\ Support/JetBrains/*/ssl/cacerts)
+do
+	if [ -f "${keystore}" ]
+	then
+		keytool -importcert -alias Zscaler-Root-CA -keystore "${keystore}" -trustcacerts -file ~/.certs/Zscaler-Root-CA.pem -storepass changeit -noprompt
+	fi
+done
+
+if [ -f /Applications/DBeaver.app/Contents/Eclipse/jre/Contents/Home/bin/keytool ]
+then
+	/Applications/DBeaver.app/Contents/Eclipse/jre/Contents/Home/bin/keytool -cacerts -importcert -alias Zscaler-Root-CA -file ~/.certs/Zscaler-Root-CA.pem -storepass changeit -noprompt
+fi
